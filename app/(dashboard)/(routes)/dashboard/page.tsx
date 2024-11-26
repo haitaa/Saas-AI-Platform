1beb236048a8af33cd24c3a7c7b01407d16dc83c
haitaa <mustafahaitaa@gmail.com>
2024-11-26 19:44:22 +0200

    Sidebar component

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
-
 const Dashboard = () => {
   return (
     <div>
       <p>Dashboard Page (Unprotected)</p>
-      <UserButton />
     </div>
   );
 };
